14f554a7ddd2f9213c771592f9db2f054c541af2
Dell <jonathanphy1210@gmail.com>
2024-11-04 11:40:34 +0530

    create admin interface, add items in menu.

--- restaurant_menu/admin.py
+++ restaurant_menu/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . models import Item
 
-# Register your models here.
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('meal', 'status')
+    list_filter = ('status',)
+    search_fields =('meals', 'description')
+
+
+admin.site.register(Item, MenuAdmin)
