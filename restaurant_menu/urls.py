2b79dbfc907a1722946210cb345698ffde2afb29
Dell <jonathanphy1210@gmail.com>
2024-11-04 12:38:48 +0530

    add html home page,description page.

--- restaurant_menu/urls.py
+++ restaurant_menu/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
-from . views import MenuList, MenuItemDetails
+
 
 urlpatterns = [
-    path('', views.MenuList.as_view(), name='MenuList'),
-    path('', views.MenuItemDetails.as_view(), name='MenuItemDetails'),
+    path('', views.MenuList.as_view(), name='home'),
+    path('item/<int:pk>/', views.MenuItemDetails.as_view(), name='menu_detail'),
 ]
